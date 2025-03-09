3b5112ff152a66a85e89ec068888ccc3369c7a91
RefinedDev <aryamkkumar2005@gmail.com>
2025-03-09 12:01:28 +0530

    web build

---
+++ web/out/raytrace_2d_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h8dc3e91eec34927d: (a: number, b: number, c: number) => void;
+export const closure2764_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hd5ab4d89df9d640c: (a: number, b: number) => void;
+export const closure2761_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h9ee0f3afefa4d8ce: (a: number, b: number) => void;
+export const closure99828_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
